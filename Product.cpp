b7117710ff3d3bab44846af0e78b67dfc235aebd
dac <dac@dypm.org>
2023-10-19 15:35:48 +0530

    Changes done

--- Product.cpp
+++ Product.cpp
@@ -1,7 +1,7 @@
 #include "Product.h"
 #include<string>
 using namespace std;
-Product::Product(int id,string name,string desc,double price)
+Product::Product(int id ,string name,string desc,double price)
 {
 	this->id = id;
 	this->name = name;
