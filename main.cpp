2de45b677daf4985d86c6b0049c8ee07f2d27e82
dac <dac@dypm.org>
2023-10-19 15:33:06 +0530

    changes done

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main() 
+int main()  
 {
 	int id,choice;
 	std::string name,desc;
